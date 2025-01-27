455852d8160af8590d0d9baac78c147293137dc7
“IzzyHunter” <izzy@sensi.fit>
2025-01-27 16:19:39 -0500

    tempwork complete

---
+++ index.js
@@ -0,0 +1,25 @@
+function convertToCelsius(f) {
+  const c = (f - 32) * (5 / 9);
+  return c;
+}
+
+function describeTemperature(f) {
+  const c = convertToCelsius(f);
+  let description = "";
+  if (c < 0) {
+    description = "very cold ";
+  } else if (c < 20) {
+    description = "cold";
+  } else if (c < 30) {
+    description = "warm";
+  } else if (c < 40) {
+    description = "hot";
+  } else {
+    description = "very hot";
+  }
+  return `${f}°F is ${c}°C. That is ${description}.`;
+}
+
+const f = prompt("Please enter a temperature in Fahrenheit");
+const description = describeTemperature(f);
+alert(description);
